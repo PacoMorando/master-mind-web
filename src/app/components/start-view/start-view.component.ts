8f043fd60a68856a6609b57b38b35dc67edf1db5
NYP_PC <paco.morando@gmail.com>
2023-09-04 19:15:48 -0600

    Open Game obtiene array del backend

--- src/app/components/start-view/start-view.component.ts
+++ src/app/components/start-view/start-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MastermindSessionService } from 'src/app/services/mastermind-session.service';
+import { Observable } from 'rxjs';
 import { StartService } from 'src/app/services/start.service';
 
 @Component({
@@ -8,19 +8,33 @@ import { StartService } from 'src/app/services/start.service';
   styleUrls: ['./start-view.component.css']
 })
 export class StartViewComponent implements OnInit {
+  // protected gamesNames: string[] = new Array();
+  protected gamesNamesTest: string[] = [];
 
   constructor(private startService: StartService) {
   }
-  // constructor(private mastermindService: MastermindSessionService) {
-  // }
 
   ngOnInit(): void {
     this.startService.getStartView();
     console.log('Se ejecuto el OnInit');
+    // console.log(this.gamesNames);
+    this.startService.getGamesNamesTest().subscribe(data => {
+      this.gamesNamesTest = data;
+      console.log(this.gamesNamesTest);
+    });
   }
 
   protected newGame() {
     this.startService.newGame();
   }
-  
+
+  // protected setGamesNames() {
+  //   console.log(this.startService.getGamesNames());
+  //   this.gamesNames = this.startService.getGamesNames();
+  // }
+
+  protected nameClicked(name: string) {
+    console.log(`Clickaste: ${name}`);
+  }
+
 }
--- src/app/services/start.service.ts
+++ src/app/services/start.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionDTO } from './session-dto';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 import { ResponseSession } from './response-session';
 
 @Injectable({
@@ -12,13 +12,22 @@ export class StartService {
   private baseUrl = 'http://localhost:8080/mastermind';
   public sessionDTO!: SessionDTO;
   storage: Storage = sessionStorage;
+  // protected gamesNames: string[] = new Array();
+  protected gamesNamesTest!:  BehaviorSubject <string[]>;
 
   constructor(private httpClient: HttpClient, private router: Router) {
+  //  this.gamesNamesTest = this.getGamesNamesTest()
   }
 
   public getStartView() {
     this.storage.clear();
     this.httpClient.get(`${this.baseUrl}/main`).subscribe();
+    // this.httpClient.get<string[]>(`${this.baseUrl}/start/getGamesNames`).subscribe(
+    //   response => {
+    //     // this.gamesNames = response;
+    //     console.log(response);
+      
+    //   });
   }
 
   public newGame() {
@@ -33,8 +42,21 @@ export class StartService {
 
   private getNewGame(): Observable<SessionDTO> {
     return this.httpClient.get<ResponseSession>(`${this.baseUrl}/start/newGame`).pipe(
-      map(reponse => reponse)
+      map(reponse => reponse)//Auqi no se si estoy seguro que debo hacer este map
     );
   }
-  
+
+  // public getGamesNames(): string[] {
+  //   return this.gamesNames;
+  // }
+
+  public getGamesNamesTest(): Observable<string[]> {
+   return this.httpClient.get<string[]>(`${this.baseUrl}/start/getGamesNames`).pipe(
+      map (response => response));
+  }
+
+
+  get gamesNamesTestAsObservable(){
+    return this.gamesNamesTest.asObservable();
+  }
 }
